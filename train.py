f655da14e95de7a5a12aec8f28e520106f4e4017
Mehak Sharma <mehak1815719@gmail.com>
2026-06-29 10:47:08 +0200

    Fix class order, hardcoded paths and update gitignore

--- train.py
+++ train.py
@@ -2,9 +2,8 @@ from ultralytics import YOLO
 
 model = YOLO('yolo11n.pt')
 model.train(
-    #data="./path_to_data_yaml.yaml",
-    data='/home/aisd_user12/yolo_config.yaml',
+    data='yolo_config.yaml',
     epochs=100,
     imgsz=640,
     batch=16
-) 
+)
